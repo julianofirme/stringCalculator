39118c4dbfec092d0586f8ae58c17ee99fc3f067
jfirme-sys <julianofirme23@gmail.com>
2021-07-20 01:55:38 -0300

    RED - return string as number

--- tests/stringCalculator.spec.js
+++ tests/stringCalculator.spec.js
@@ -9,4 +9,8 @@ describe("string calculator", () => {
     it("should be return 0 to empty string", () => {
         expect(stringCalculator.getNumbers('')).to.equal(0);
     });
+
+    it("should be return string as number", () => {
+        expect(stringCalculator.getNumbers('1')).to.equal(1);
+    });
 })
