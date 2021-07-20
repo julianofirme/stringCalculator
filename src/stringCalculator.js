f653097d9731fbe387ec46fd0e9302869ee98eed
jfirme-sys <julianofirme23@gmail.com>
2021-07-20 09:15:58 -0300

    GREEN - return string as number

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,9 @@
 function getNumbers(numberString) {
     if(numberString === '') return 0;
+    
+    if(typeof numberString === "string") {
+        return parseFloat(numberString);
+    }
 
     return numberString;
 }
