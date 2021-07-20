cdd02dc7bd575913ba11454ea655d7b61fc50be0
jfirme-sys <julianofirme23@gmail.com>
2021-07-20 01:36:14 -0300

    RED - return number 1

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,4 +1,4 @@
-function getNumbers(num) {
+function getNumbers() {
 
 }
 
--- tests/stringCalculator.spec.js
+++ tests/stringCalculator.spec.js
@@ -2,5 +2,7 @@ const { expect } = require("chai");
 const stringCalculator = require('../src/stringCalculator');
 
 describe("string calculator", () => {
-
+    it("should be return 1", () => {
+        expect(stringCalculator.getNumbers(1)).to.equal(1);
+    });
 })
