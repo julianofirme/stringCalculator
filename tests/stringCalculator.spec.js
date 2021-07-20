426fd1312ef7d87f941731472a9503f7f145200f
jfirme-sys <julianofirme23@gmail.com>
2021-07-20 01:45:22 -0300

    RED - return 0 to empty string

--- tests/stringCalculator.spec.js
+++ tests/stringCalculator.spec.js
@@ -5,4 +5,8 @@ describe("string calculator", () => {
     it("should be return 1", () => {
         expect(stringCalculator.getNumbers(1)).to.equal(1);
     });
+
+    it("should be return 0 to empty string", () => {
+        expect(stringCalculator.getNumbers('')).to.equal(0);
+    });
 })
